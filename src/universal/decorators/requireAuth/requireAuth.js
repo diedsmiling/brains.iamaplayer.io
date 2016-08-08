919f00653f34dc489a970bfa8018e2d1a0aa3b45
lazarev <diedsmiling@gmail.com>
2016-08-08 23:32:30 +0300

    Initial commit

---
+++ src/universal/decorators/requireAuth/requireAuth.js
@@ -0,0 +1,57 @@
+import React, {PropTypes, Component} from 'react';
+import {push} from 'react-router-redux';
+import socketOptions from 'universal/utils/socketOptions';
+
+let key;
+
+const isLoginUrl = location => {
+  return location.pathname === '/login'
+}
+
+export default ComposedComponent => {
+  return class RequiredAuth extends Component {
+    static propTypes = {
+      isAuthenticated: PropTypes.bool,
+      dispatch: PropTypes.func,
+      hasAuthError: PropTypes.bool,
+      location: PropTypes.shape({
+        query: PropTypes.shape({
+          e: PropTypes.string,
+          next: PropTypes.string
+        })
+      })
+    }
+
+    componentWillMount() {
+      this.checkForAuth(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      this.checkForAuth(nextProps);
+    }
+
+    render() {
+      const {isAuthenticated, location} = this.props;
+      console.log(location)
+      if (isAuthenticated || isLoginUrl(location)) {
+        return <ComposedComponent {...this.props}/>;
+      }
+      return <div>Logging in...</div>;
+    }
+
+    checkForAuth(props) {
+      if (__CLIENT__) {
+        const {dispatch, hasAuthError, location} = props;
+        const newKey = location && location.key || 'none';
+        if (newKey === key) {
+          return;
+        }
+        key = newKey;
+        const authToken = localStorage.getItem(socketOptions.authTokenName);
+        if ((hasAuthError || !authToken) && !isLoginUrl(location)) {
+          dispatch(push('/login?next=%2Fkanban'));
+        }
+      }
+    }
+  };
+};
---
+++ src/universal/modules/landing/containers/Landing/LandingContainer.js
@@ -0,0 +1,24 @@
+import React, {PropTypes, Component} from 'react';
+import Landing from 'universal/modules/landing/components/Landing/Landing';
+import {connect} from 'react-redux';
+import {ensureState} from 'redux-optimistic-ui';
+import requireAuth from 'universal/decorators/requireAuth/requireAuth';
+
+@connect(mapStateToProps)
+@requireAuth
+export default class LandingContainer extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
+  };
+
+  render() {
+    return <Landing {...this.props}/>;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: ensureState(state).getIn(['auth', 'isAuthenticated'])
+  };
+}
